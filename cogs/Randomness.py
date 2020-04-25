7793252e5f0fb3322457e02bfd0480a03dab4b2d
Vexrax <joshuaf_111@live.com>
2020-04-24 20:05:46 -0400

    adding some randomness commands

---
+++ cogs/Randomness.py
@@ -0,0 +1,25 @@
+import discord
+import random
+from discord.ext import commands
+
+class Randomness(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Randomness Command Ready")
+
+    @commands.command()
+    async def flipcoin(self, ctx):
+        await ctx.send(f'Coin landed on {random.choice(["Heads", "Tails"])}')
+
+    @commands.command()
+    async def rolldice(self, ctx):
+        await ctx.send(f'Dice rolled a {random.randint(1,6)}')
+
+        
+
+def setup(client):
+    client.add_cog(Randomness(client))
