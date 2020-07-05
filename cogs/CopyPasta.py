da706fb06e9ccd8ae1459e527b5aef494f21c653
Vexrax <joshuaf_111@live.com>
2020-07-04 23:08:23 -0400

    fix pasta

--- cogs/CopyPasta.py
+++ cogs/CopyPasta.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 class CopyPasta(commands.Cog):
 
-    copypasta =  [
+    pastas =  [
         "If Earleking has million number of fans i am one of them. if Earleking has ten fans i am one of them. if Earleking has no fans. that means i am no more on the earth. if world against Earleking , i am against the world. i love Earleking till my last breath... die hard fan of Earleking . Hit like if u think Earleking best & smart in the world",
         "Earleking makes: \n my rod needlessly large \n my wand blast \n my phantom dance \n my cannon rapidly fire \n my tome amplify \n my hydra ravenous \n my spellbook unseal"
     ]
@@ -18,7 +18,7 @@ class CopyPasta(commands.Cog):
 
     @commands.command()
     async def copypasta(self, ctx):
-        for pasta in self.copypasta:
+        for pasta in self.pastas:
             await ctx.send(pasta)
 
 def setup(client):
