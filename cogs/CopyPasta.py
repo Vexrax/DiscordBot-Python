0b6b08e2c635e0be08bed9c745879d0e26da0d63
Vexrax <joshuaf_111@live.com>
2020-07-23 13:07:08 -0400

    Add pasta and reorder reqs to force fresh pull from pip

--- cogs/CopyPasta.py
+++ cogs/CopyPasta.py
@@ -6,7 +6,8 @@ class CopyPasta(commands.Cog):
 
     pastas =  [
         "If Earleking has million number of fans i am one of them. if Earleking has ten fans i am one of them. if Earleking has no fans. that means i am no more on the earth. if world against Earleking , i am against the world. i love Earleking till my last breath... die hard fan of Earleking . Hit like if u think Earleking best & smart in the world",
-        "Earleking makes: \n my rod needlessly large \n my wand blast \n my phantom dance \n my cannon rapidly fire \n my tome amplify \n my hydra ravenous \n my spellbook unseal"
+        "Earleking makes: \n my rod needlessly large \n my wand blast \n my phantom dance \n my cannon rapidly fire \n my tome amplify \n my hydra ravenous \n my spellbook unseal",
+        "Earleking fired up his stream for another day of soloQ, but foolishly forgot to check if Vexrax was in a game. He opened up Vexrax’s stream to find him sitting in queue… He frantically began tabbing between his client and Vexrax’s stream, then one after the other… both queues popped. “I need the toilet” he said to his chat. He ran to the wall and unplugged his internet. He was safe for another day."
     ]
 
     def __init__(self, client):
